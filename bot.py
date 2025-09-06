e50572901fd02bd8ea3e30cc8e6331afc6b0c291
Blake <117682805+BlakeSpendlove@users.noreply.github.com>
2025-09-06 08:31:29 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ from discord.ext import commands, tasks
 import random
 
 intents = discord.Intents.default()
+intents.message_content = True
 intents.presences = True
 intents.guilds = True
 intents.members = True
